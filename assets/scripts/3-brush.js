b6e51385741d13b4d82f3c98d43a53c13bfb498b
Moses Openja <mosesopenja@Mosess-MacBook-Air.local>
2020-05-16 20:35:34 -0400

    Ressolved the merge conflict, now good to go

---
+++ assets/scripts/3-brush.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+* File to handle zoom/brush
+*/
+
+
+/**
+ * Allows to redraw the "focus" line chart based on the selected zome in the "conext" line chart
+ *
+ * @param brush     The selection zone in the context graphic
+ * @param g         The SVG group in which the focus line chart is drawn
+ * @param line      The function allowing to draw the lines of the graphic
+ * @param xFocus    The X scale for the focus graphic
+ * @param xContext  The X scale for the context graphic
+ * @param xAxis     The X axis for the focus line chart
+ * @param yAxis     The Y axis for the focus line chart
+ *
+ * @see https://bl.ocks.org/mbostock/34f08d5e11952a80609169b7917d4172
+ */
+function brushUpdate(brush, g, line, xFocus, xContext, xAxis, yAxis) {
+  // TODO: Redraw the focus line chart as a function of the selected zone in the context line chart
+  var focs = d3.event.selection || xContext.range()
+  var invt = xContext.invert
+  xFocus.domain(focs.map(invt, xContext))
+  var streets = g.selectAll(".line")
+  streets.attr("d", line)
+  var axis = d3.select(".x.axis")
+  axis.call(xAxis)
+}
---
+++ assets/scripts/4-legend.js
@@ -0,0 +1,74 @@
+"use strict";
+
+/**
+ * File used for generating the legend and controlling the interactions with it.
+ */
+
+
+/**
+ * Create a legend from the given source.
+ *
+ * @param svg       SVG element to use in order to create the legend.
+ * @param sources   Data sorted by street name and by date.
+ * @param color     The 10-color scale to use.
+ */
+function legend(svg, sources, color) {
+  // TODO: Create the legend that supplements the graphic.
+  var node = svg.node()
+  var width = node.getAttribute("width")
+  var height = node.getAttribute("height")
+
+  var legend = svg.append("g")
+    .selectAll("g")
+    .data(sources)
+    .enter()
+    .append('g')
+      .attr('class', 'legend')
+      .attr('transform', function(d, i) {
+        var heightCase = height / (sources.length*2)
+        var x = width / 18;
+        var y = 30 + i * heightCase;
+        return 'translate(' + x + ',' + y + ')'
+      })
+      .on("click", function(d) {
+        displayLine(this, color)
+      })
+
+    legend.append("rect")
+    .attr('width', 0.01*width)
+    .attr('height', 0.025*height)
+    .style('stroke', "black")
+    .style('fill', function(d) {
+      return d.name === "Moyenne" ? "#000000" : color(d.name)
+    })
+    legend.append('text')
+    .attr('x', 20)
+    .attr('y', 10)
+    .text(function(d) { return d.name })
+}
+
+/**
+ * Allows for show/hide whether the line that corresponding to the clicked square.
+ *
+ * By clicking on a square, we display/hide the corresponding line and the square's interior becomes white/goes back to its original color.
+ *
+ * @param element   The square that was clicked
+ * @param color     The 10-color scale
+ */
+function displayLine(element, color) {
+  // TODO: Complete the code to show or hide a line depending on the selected item
+  var cell = element.childNodes[0]
+  var text = element.childNodes[1].childNodes[0].data
+  var whitenRect = "rgb(255, 255, 255)"
+  var Focuslines = d3.select("#" + text).node().style
+  var Contextlines = d3.select("#" + text + "Context").node().style
+  if (cell.style.fill == whitenRect) {
+    cell.style.fill = (text === "Moyenne" ? "#000000" : color(text))
+    Focuslines.opacity = 1
+    Contextlines.opacity = 1
+  } else {
+    cell.style.fill = whitenRect
+    Focuslines.opacity = 0
+    Contextlines.opacity = 0
+  }
+}
